a5e2ffa6dc2e185f0dd4c0208429696a7caf6de8
Luca Pacher <pacher@to.infn.it>
2017-09-04 22:38:11 +0200

    Create GUI.py

---
+++ pychipix/gui/GUI.py
@@ -0,0 +1,25 @@
+
+#----------------------------------------------------------------------------------------------------
+#                                        VLSI Design Laboratory
+#                               Istituto Nazionale di Fisica Nucleare (INFN)
+#                                   via Giuria 1 10125, Torino, Italy
+#----------------------------------------------------------------------------------------------------
+# {Trace}
+# [Filename]      GUI.py [CLASS]
+# [Project]       CHIPIX65 demonstrator PyROOT-based DAQ
+# [Author]        Luca Pacher - pacher@to.infn.it
+# [Version]       1.0
+# [Language]      Python/ROOT
+# [Created]       Sep 3, 2017
+# [Description]   Just a class used as a namespace, actual objectes are created by user interaction 
+# [Notes]         -
+# {Trace}
+#----------------------------------------------------------------------------------------------------
+
+
+class GUI(object) :
+
+	bar             = None
+	TestCommandsGui = None
+	PixelScanGui    = None
+
