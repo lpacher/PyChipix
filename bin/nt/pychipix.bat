f5cc3045fa481367a6f57ce267046f472361d8e5
Luca Pacher <pacher@to.infn.it>
2017-09-04 22:33:27 +0200

    Update pychipix.bat

--- bin/nt/pychipix.bat
+++ bin/nt/pychipix.bat
@@ -1,4 +1,32 @@
 
 @echo off
 
-python -i -O pychipix %*
+
+:: parse here the command line help
+if "%1"=="--help" (
+
+   echo.
+   echo Command line usage:
+   echo.
+   echo pychipix [[--batch^|--gui] ^| [script.py]]
+   echo.
+   echo Options:
+   echo.
+   echo  --batch : run in batch mode without graphics
+   echo  --gui   : start interactive session and open the TControlBar 
+   echo.
+
+   goto end
+
+) else (
+
+   goto start
+
+)
+
+
+:start
+
+python -i -O pychipix %* || exit
+
+:end
